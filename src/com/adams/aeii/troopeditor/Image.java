0ce79b1e2da4c5848a453172edd567387cfe92a0
s1407003 <s1407003@WSH-S1407003.wkscn.wistron>
2014-09-22 16:59:01 +0800

    Audio_Test Update

--- src/com/adams/aeii/troopeditor/Image.java
+++ src/com/adams/aeii/troopeditor/Image.java
@@ -36,7 +36,7 @@ public final class Image {
     }
 
     public void getUnits() throws IOException {
-        BufferedImage img_unit = ImageIO.read(new File("res\\image\\unit\\unit_icons.png"));
+        BufferedImage img_unit = ImageIO.read(new File("res\\img\\units\\unit_icons_0.png"));
         units = new BufferedImage[12];
         for (i = 0; i < unit_count; i++) {
             x = 24 * i;
